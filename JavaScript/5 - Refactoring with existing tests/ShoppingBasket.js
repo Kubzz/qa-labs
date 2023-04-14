4b78648c39334e468f0c76d994766b386a968517
Kubzz <kallchorne@gmail.com>
2023-04-14 09:54:33 +0100

    lab 5 updated

--- JavaScript/5 - Refactoring with existing tests/ShoppingBasket.js
+++ JavaScript/5 - Refactoring with existing tests/ShoppingBasket.js
@@ -13,8 +13,9 @@ const printBasket = basket => {
                     basketOutput += `${priceToAdd}\n`;
                     total += parseFloat(priceToAdd);
                     break;
-                case `_id`:
-                case `_vat`:
+                // case `_id`:
+                // case `_vat`:
+                // redundant case statements
                 default:
                     break;
             }
@@ -24,22 +25,35 @@ const printBasket = basket => {
     return basketOutput;
 }
 
+//add all objects to a list and then added them one by one with a for loop to basket
+
+const items = [{ _id: 1, _name: "Baked Beans", _price: 0.85, _vat: false },
+                { _id: 2, _name: "Battenburg Cake", _price: 1, _vat: true },
+                { _id: 3, _name: "Granary Loaf", _price: 1.45, _vat: false },
+                { _id: 4, _name: "Bottle of Red Wine", _price: 11.05, _vat: true }
+            ]
+
 const myBasket = [];
 
-const item1 = { _id: 1, _name: "Baked Beans", _price: 0.85, _vat: false };
-myBasket.push(item1);
-console.log(printBasket(myBasket));
+items.forEach(item => {
+    myBasket.push(item);
+    console.log(printBasket(myBasket));
+});
+            
+// const item1 = { _id: 1, _name: "Baked Beans", _price: 0.85, _vat: false };
+// myBasket.push(item1);
+// console.log(printBasket(myBasket));
 
-const item2 = { _id: 2, _name: "Battenburg Cake", _price: 1, _vat: true };
-myBasket.push(item2);
-console.log(printBasket(myBasket));
+// const item2 = { _id: 2, _name: "Battenburg Cake", _price: 1, _vat: true };
+// myBasket.push(item2);
+// console.log(printBasket(myBasket));
 
-const item3 = { _id: 3, _name: "Granary Loaf", _price: 1.45, _vat: false };
-myBasket.push(item3);
-console.log(printBasket(myBasket));
+// const item3 = { _id: 3, _name: "Granary Loaf", _price: 1.45, _vat: false };
+// myBasket.push(item3);
+// console.log(printBasket(myBasket));
 
-const item4 = { _id: 4, _name: "Bottle of Red Wine", _price: 11.05, _vat: true };
-myBasket.push(item4);
-console.log(printBasket(myBasket));
+// const item4 = { _id: 4, _name: "Bottle of Red Wine", _price: 11.05, _vat: true };
+// myBasket.push(item4);
+// console.log(printBasket(myBasket));
 
 module.exports = printBasket;
