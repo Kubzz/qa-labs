5a1b9f8f4de4ba5e0aaba73f76e7b59dd9a74f36
Kubzz <kallchorne@gmail.com>
2023-04-14 10:28:26 +0100

    lab 6.1 completed (java)

--- Java/6.1 - Test driven development (TDD)/src/main/java/com/agile/calculator/Calculator.java
+++ Java/6.1 - Test driven development (TDD)/src/main/java/com/agile/calculator/Calculator.java
@@ -44,4 +44,25 @@ public class Calculator {
 		result = Utils.toNumber(numbers[0]) - Utils.toNumber(numbers[1]);
 		return result;
 	}
+
+	public int add(String n1) {
+		int result = 0;
+		
+		if (n1.equals("")) {
+			return 0;
+		} else if (!(n1.contains(",") || n1.contains("\n"))) {
+			return Utils.toNumber(n1);
+		}
+		String n2 = n1.replace("\n", ",");
+		String[] numbers = n2.split(",");
+		for(String number : numbers) {
+			if(!Utils.isNumeric(number)) {  // check number is valid
+				return -9999;
+			} else if (Utils.toNumber(number) < 0) {
+				throw new ArithmeticException("Negative numbers not allowed");
+			}
+			result += Utils.toNumber(number);
+		}
+		return result;
+	}
 }
--- Java/6.1 - Test driven development (TDD)/src/test/java/com/agile/calculator/CalculatorTest.java
+++ Java/6.1 - Test driven development (TDD)/src/test/java/com/agile/calculator/CalculatorTest.java
@@ -70,4 +70,50 @@ public class CalculatorTest {
 		calculator.divide("10,0");
 	}
 
+	@Test
+	public void emptyStrReturnsZero() {
+		calculator = new Calculator();
+		int expected = 0;
+		int actual = calculator.add("");
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void singleNumberReturned() {
+		calculator = new Calculator();
+		int expected = 1;
+		int actual = calculator.add("1");
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void twoNumbersAdded() {
+		calculator = new Calculator();
+		int expected = 5;
+		int actual = calculator.add("2,3");
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void twoNumbersAddedSeperatedByNewline() {
+		calculator = new Calculator();
+		int expected = 5;
+		int actual = calculator.add("2\n3");
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void threeNumbersAdded() {
+		calculator = new Calculator();
+		int expected = 9;
+		int actual = calculator.add("2\n3,4");
+		assertEquals(expected, actual);
+	}
+
+	@Test(expected = java.lang.ArithmeticException.class)
+	public void negativeNumberThrowsException() {
+		calculator = new Calculator();
+		calculator.add("-1,4");
+	}
+
 }
